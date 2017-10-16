412d331846fdf4130138633203cabae27816007a
Flavien Medina <flavien@MacBook-Pro-de-Flavz.local>
2017-10-16 11:41:08 +0200

    first commit

--- js/burger.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("#nav").addClass("js");
-  $("#nav").addClass("js").before('<div id="menu">☰</div>');
-  $("#menu").click(function(){
-  	$("#nav").toggleClass('hide_menu');
-  });
-});
